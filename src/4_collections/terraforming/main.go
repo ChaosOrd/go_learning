27e6c4efce5be27ec5c5893e879edc815aa6e592
pavelryb <pavel@traxretail.com>
2019-04-21 14:05:01 -0700

    completed terraforming exercise

--- src/4_collections/terraforming/main.go
+++ src/4_collections/terraforming/main.go
@@ -2,15 +2,23 @@ package main
 
 import (
 	"fmt"
-	"sort"
 )
 
+type Planets []string
+
+func (p Planets) Terraform() {
+
+	for idx, planet := range p {
+		p[idx] = "New" + planet
+	}
+}
 
 func main() {
 	planets := []string{
 		"Mercury", "Venus", "Earth", "Mars",
 		"Jupiter", "Saturn", "Uranus", "Neptune",
 	}
-	sort.StringSlice(planets).Sort()
+
+	Planets(planets).Terraform()
 	fmt.Println(planets)
 }
